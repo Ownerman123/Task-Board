b8a5c9beafd9364c0ed3d6036678e46e5498dde7
Ownerman123 <charlesshumway1@gmail.com>
2024-03-29 18:04:20 -0600

    add card builder

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,15 +3,54 @@ let taskList = JSON.parse(localStorage.getItem("tasks"));
 let nextId = JSON.parse(localStorage.getItem("nextId"));
 
 const modal = $("#formModal");
+const today = dayjs();
+const todaysDate = $("#current-date");
+todaysDate.text("Todays date is: " + dayjs(today).format("MM/D/YYYY"));
 
 
-// Todo: create a function to generate a unique task id
+
 function generateTaskId() {
+    const id = crypto.randomUUID();
+    return id;
 
 }
 
 // Todo: create a function to create a task card
 function createTaskCard(task) {
+
+    const cardContainer = $('div').attr('data-task-id', task.id).addClass('fluid-container card task-card draggable my-3');
+
+    const cardHeader = $('h5').addClass('card-header h4').text(task.title);
+
+    const cardBody = $('div').addClass('card-body');
+    
+    const cardDescription = $('p').addClass('card-text').text(task.description);
+    
+    const cardDueDate = $('p').addClass('card-text').text(task.date);
+    
+    const cardBtn = $('button')
+    .addClass('btn btn-danger delete')
+    .text('Delete')
+    .attr('data-task-id', task.id);
+   
+
+    if (task.dueDate && task.status !== 'done') {
+        const now = dayjs();
+        const taskDueDate = dayjs(task.dueDate, 'DD/MM/YYYY');
+    }
+
+    if (now.isSame(taskDueDate, 'day')) {
+        cardContainer.addClass('bg-warning text-white');
+      } else if (now.isAfter(taskDueDate)) {
+        cardContainer.addClass('bg-danger text-white');
+        cardBtn.addClass('border-light');
+      }
+
+      cardBody.append(cardDescription, cardDueDate, cardBtn);
+      cardContainer.append(cardHeader, cardBody);
+      return cardContainer;
+   
+    
 
 }
 
@@ -22,6 +61,8 @@ function renderTaskList() {
 
 // Todo: create a function to handle adding a new task
 function handleAddTask(event){
+
+
 
 }
 
@@ -37,5 +78,18 @@ function handleDrop(event, ui) {
 
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
 $(document).ready(function () {
+    renderTaskList();
+
+    $('#due-date').datepicker({
+        changeMonth: true,
+        changeYear: true,
+      });
+    
+      // ? Make lanes droppable
+      $('.lane').droppable({
+        accept: '.draggable',
+        drop: handleDrop,
+      });
+
 
 });
