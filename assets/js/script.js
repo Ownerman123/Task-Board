03e3af0ad9c87e180f18f9a933aa3d58d857f4e9
Ownerman123 <charlesshumway1@gmail.com>
2024-04-01 10:17:42 -0600

    add card creation and deletion functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,12 @@ let taskList = JSON.parse(localStorage.getItem("tasks"));
 let nextId = JSON.parse(localStorage.getItem("nextId"));
 
 const modal = $("#formModal");
+const taskFormEl = $('#form-container');
+const titleInputEl = $('#task');
+const dueDateInputEl = $('#due-date');
+const descriptionInputEl = $('#task-desc');
+const submitBtn = $('#submit-btn');
+
 const today = dayjs();
 const todaysDate = $("#current-date");
 todaysDate.text("Todays date is: " + dayjs(today).format("MM/D/YYYY"));
@@ -15,20 +21,42 @@ function generateTaskId() {
 
 }
 
+function readTasksInStorage() {
+  
+  
+  let tasks = JSON.parse(localStorage.getItem('tasks'));
+
+  
+  if (!tasks) {
+    tasks = [];
+  }
+
+  
+  return tasks;
+}
+
+function saveTasksToStorage(tasks) {
+
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+
+  
+
+}
+
 // Todo: create a function to create a task card
 function createTaskCard(task) {
 
-    const cardContainer = $('div').attr('data-task-id', task.id).addClass('fluid-container card task-card draggable my-3');
+    const cardContainer = $('<div>').attr('data-task-id', task.id).addClass('fluid-container card task-card draggable my-3');
 
-    const cardHeader = $('h5').addClass('card-header h4').text(task.title);
+    const cardHeader = $('<h5>').addClass('card-header h4').text(task.title);
 
-    const cardBody = $('div').addClass('card-body');
+    const cardBody = $('<div>').addClass('card-body');
     
-    const cardDescription = $('p').addClass('card-text').text(task.description);
+    const cardDescription = $('<p>').addClass('card-text').text(task.description);
     
-    const cardDueDate = $('p').addClass('card-text').text(task.date);
+    const cardDueDate = $('<p>').addClass('card-text').text(task.date);
     
-    const cardBtn = $('button')
+    const cardBtn = $('<button>')
     .addClass('btn btn-danger delete')
     .text('Delete')
     .attr('data-task-id', task.id);
@@ -36,15 +64,17 @@ function createTaskCard(task) {
 
     if (task.dueDate && task.status !== 'done') {
         const now = dayjs();
-        const taskDueDate = dayjs(task.dueDate, 'DD/MM/YYYY');
+        const taskDueDate = dayjs(task.date, 'DD/MM/YYYY');
     }
 
-    if (now.isSame(taskDueDate, 'day')) {
+    if (today.isSame(task.date, 'day')) {
         cardContainer.addClass('bg-warning text-white');
-      } else if (now.isAfter(taskDueDate)) {
+      } else if (today.isAfter(task.dueDate)) {
         cardContainer.addClass('bg-danger text-white');
         cardBtn.addClass('border-light');
       }
+
+      cardBtn.on('click', handleDeleteTask)
 
       cardBody.append(cardDescription, cardDueDate, cardBtn);
       cardContainer.append(cardHeader, cardBody);
@@ -57,18 +87,95 @@ function createTaskCard(task) {
 // Todo: create a function to render the task list and make cards draggable
 function renderTaskList() {
 
+  const tasks = readTasksInStorage();
+  
+  const todoList = $('#todo-cards');
+  todoList.empty();
+
+  const inProgressList = $('#in-progress-cards');
+  inProgressList.empty();
+
+  const doneList = $('#done-cards');
+  doneList.empty();
+
+  for (let task of tasks) {
+    if (task.status === 'to-do') {
+      todoList.append(createTaskCard(task));
+    } else if (task.status === 'in-progress') {
+      inProgressList.append(createTaskCard(task));
+    } else if (task.status === 'done') {
+      doneList.append(createTaskCard(task));
+    }
+  }
+
+  $('.draggable').draggable({
+    opacity: 0.7,
+    zIndex: 100,
+    // ? This is the function that creates the clone of the card that is dragged. This is purely visual and does not affect the data.
+    helper: function (e) {
+      // ? Check if the target of the drag event is the card itself or a child element. If it is the card itself, clone it, otherwise find the parent card  that is draggable and clone that.
+      const original = $(e.target).hasClass('ui-draggable')
+        ? $(e.target)
+        : $(e.target).closest('.ui-draggable');
+      // ? Return the clone with the width set to the width of the original card. This is so the clone does not take up the entire width of the lane. This is to also fix a visual bug where the card shrinks as it's dragged to the right.
+      return original.clone().css({
+        width: original.outerWidth(),
+      });
+    },
+  });
+
+
+
 }
 
 // Todo: create a function to handle adding a new task
 function handleAddTask(event){
 
+  event.preventDefault()
+  console.log('task being handled baby')
 
+  const taskTitle = titleInputEl.val().trim();
+  const taskDescription = descriptionInputEl.val(); 
+  const taskDueDate = dueDateInputEl.val(); 
+
+  const newTask = {
+        
+    id: generateTaskId(),
+    title: taskTitle,
+    description: taskDescription,
+    date: taskDueDate,
+    status: 'to-do',
+  }
+
+  const tasks = readTasksInStorage();
+  tasks.push(newTask);
+  saveTasksToStorage(tasks);
+  renderTaskList();
+
+  // clear inputs
+  titleInputEl.val('');
+  descriptionInputEl.val('');
+  dueDateInputEl.val('');  
 
 }
 
 // Todo: create a function to handle deleting a task
 function handleDeleteTask(event){
+  const taskId = $(this).attr('data-task-id');
+  const tasks = readTasksInStorage();
 
+  // ? Remove project from the array. There is a method called `filter()` for this that is better suited which we will go over in a later activity. For now, we will use a `forEach()` loop to remove the project.
+  tasks.forEach((task) => {
+    if (task.id === taskId) {
+      tasks.splice(tasks.indexOf(task), 1);
+    }
+  });
+
+  // ? We will use our helper function to save the projects to localStorage
+  saveTasksToStorage(tasks);
+
+  // ? Here we use our other function to print projects back to the screen
+ renderTaskList();
 }
 
 // Todo: create a function to handle dropping a task into a new status lane
@@ -93,3 +200,5 @@ $(document).ready(function () {
 
 
 });
+
+submitBtn.on('click', handleAddTask);
